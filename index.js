a3f2615574a128a9075ff4482f6c846df6f0792c
nicole pizzano <pizzano.nicole@gmail.com>
2022-11-27 17:56:45 -0500

    relaxer vanilla js

---
+++ index.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 10500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breatheAnimation();
+
+function breatheAnimation() {
+
+    text.innerText = 'Breathe In!';
+    container.className = 'container grow';
+
+    setTimeout(() => {
+        text.innerText = 'Hold';
+
+        setTimeout(() => {
+            text.innerText = 'Breathe Out!';
+            container.className = 'container shrink';
+        }, holdTime);
+    }, breatheTime);
+
+
+}
+
+setInterval(breatheAnimation, totalTime);
+
